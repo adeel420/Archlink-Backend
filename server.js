dcd33eeb3c92747db182d473c7fe7c9757198620
Your Name <your_email@example.com>
2025-06-26 19:44:31 +0500

    back

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use(express.json());
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "*", // allow all origins
+  origin: ["http://localhost:5173", "https://your-frontend.vercel.app"], // both localhost & live
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 };
 
